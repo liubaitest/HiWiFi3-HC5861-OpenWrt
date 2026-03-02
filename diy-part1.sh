c4fce83964128e1f0367bd588089f96fe823c07b
liubaitest <liubaitest@gmail.com>
2026-03-02 20:30:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,12 +24,4 @@ rm -rf package/lean/luci-theme-argon
 
 rm -rf package/lean/luci-app-jd-dailybonus
 
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb #git lua-maxminddb 依赖
 
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-
-#git clone https://github.com/fw876/helloworld.git package/helloworld #shadowsocks
-
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
